6be4143033685b08480024932db1f966e39326da
Alex Woodman <alexthegoodman@gmail.com>
2026-04-17 15:54:01 -0400

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://commonplace.social",
+  siteUrl: "https://commonplace-platform.vercel.app",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 5000,
   exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      "https://commonplace.social/server-sitemap.xml", // <==== Add here
+      "https://commonplace-platform.vercel.app/server-sitemap.xml", // <==== Add here
     ],
   },
   // ...other options
