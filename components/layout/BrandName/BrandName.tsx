b2b35129fbbcc9d3ed018f609ab2d61ed7fbeebb
Alex Woodman <alexthegoodman@gmail.com>
2026-05-10 17:43:50 -0400

    desktop styling fixes

--- components/layout/BrandName/BrandName.tsx
+++ components/layout/BrandName/BrandName.tsx
@@ -10,11 +10,14 @@ const BrandName: React.FC<BrandNameProps> = ({
   const clickHandler = (e: MouseEvent) => onClick(e);
   return (
     <div className="brandnameWrapper">
-      <span className="brandname mobileOnly">
+      {/* <span className="brandname mobileOnly">
         <img src="/logoMini.png" alt="CommonPlace Mini Logo" />
       </span>
       <span className="brandname desktopOnly">
         <img src="/wordmarkWhiteMini.png" alt="CommonPlace Logo" />
+      </span> */}
+      <span className="brandname">
+        <img src="/logoMini.png" alt="CommonPlace Mini Logo" />
       </span>
     </div>
   );
