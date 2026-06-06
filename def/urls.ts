edf1d1a692b332497f288ffd93f447f54c933594
Alex Woodman <alexthegoodman@gmail.com>
2026-06-06 14:02:26 -0400

    update for new domain

--- def/urls.ts
+++ def/urls.ts
@@ -3,21 +3,21 @@ export const env = process.env.NEXT_PUBLIC_APP_ENV;
 export const protocol = env === "production" ? "https://" : "http://";
 
 export const cookieDomain =
-  env === "production" ? "commonplace-platform.vercel.app" : "localhost";
+  env === "production" ? "commonplace.exchange" : "localhost";
 
 export const cpDomain =
   env === "production"
-    ? "commonplace-platform.vercel.app"
+    ? "commonplace.exchange"
     : process.env.NEXT_PUBLIC_DOCKER_HOST;
 
 export const cpDomainwp =
   env === "production"
-    ? "commonplace-platform.vercel.app"
+    ? "commonplace.exchange"
     : process.env.NEXT_PUBLIC_DOCKER_HOST + ":3000";
 
 export const cpGraphqlUrl =
   env === "production"
-    ? "https://commonplace-platform.vercel.app:4000/graphql"
+    ? "https://commonplace.exchange:4000/graphql"
     : `http://${process.env.NEXT_PUBLIC_DOCKER_HOST}:4000/graphql`;
 
 export const fullUrl = protocol + cookieDomain;
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://commonplace-platform.vercel.app",
+  siteUrl: "https://commonplace.exchange",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 5000,
   exclude: ["/server-sitemap.xml"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
-      "https://commonplace-platform.vercel.app/server-sitemap.xml", // <==== Add here
+      "https://commonplace.exchange/server-sitemap.xml", // <==== Add here
     ],
   },
   // ...other options
